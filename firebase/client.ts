55eefc7ea57965930cb931f289ee462a207d6f11
dylan-chua01 <98095414+dylan-chua01@users.noreply.github.com>
2025-03-29 11:41:04 +1100

    show feedback

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzJm2tx6uVevpatBSIHBeULtF4HFiXocs",
@@ -14,7 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig): getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
